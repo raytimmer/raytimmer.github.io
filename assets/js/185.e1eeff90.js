ced46f6e53859b1eaca58f9bc0b6e7ebecaab5cd
unknown <823070186@qq.com>
2023-03-16 17:21:21 +0800

    s

--- assets/js/185.e1eeff90.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[185],{1112:function(t,e,a){"use strict";a.r(e);var n={},s=a(1),o=Object(s.a)(n,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"demo"},[e("px-pagination",{attrs:{background:"",layout:"prev, pager, next",total:1e3}})],1)}),[],!1,null,"061ed4df",null);e.default=o.exports}}]);
---
+++ js/redirect-30f57524.js
@@ -0,0 +1,1 @@
+import{n}from"./main-7cbd11f9.js";const t={};var e=n({created(){const{params:n,query:t}=this.$route,{path:e}=n;this.$router.replace({path:"/"+e,query:t})},render:function(n){return n("")}},undefined,undefined,!1,r,null,null,null);function r(n){for(let e in t)this[e]=t[e]}const u=function(){return e.exports}();export{u as default};
